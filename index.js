eab3ad7e0f5f13f966a384c9688dda4495b85180
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-03-15 01:12:02 +0300

    i added an event listener > -created a handleClick function that provides the ramen detail section > -styled the image > -used the dot method to access the data in my ramen array > -called the ramendisplay function > styled the nav bar changed background colour to black and color to white

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ function displayRamens() {
     ramenImage.src = ramen.image;
 
     ramenImage.classList.add("ramen-image"); //styling class
+
+    // event listener for click on the image
+    ramenImage.addEventListener("click", () => handleClick(ramen));
+
     ramenMenuDiv.appendChild(ramenImage);
   });
 }
+
+// ramen detail display section
+function handleClick(ramen) {
+  const ramenDetailDiv = document.getElementById("ramen-detail");
+  ramenDetailDiv.innerHTML = "";
+
+  const ramenImage = document.createElement("img");
+  ramenImage.src = ramen.image;
+  ramenImage.classList.add("ramen-detail-image");
+  ramenDetailDiv.appendChild(ramenImage);
+
+  const ramenName = document.createElement("h3");
+  ramenName.textContent = ramen.name;
+
+  const ramenRestaurant = document.createElement("h3");
+  ramenRestaurant.textContent = `Restaurant: ${ramen.restaurant}`;
+
+  const ramenRating = document.createElement("p");
+  ramenRating.textContent = `Rating: ${ramen.rating}`;
+
+  const ramenComment = document.createElement("p");
+  ramenComment.textContent = `Comment: ${ramen.comment}`;
+
+  ramenDetailDiv.appendChild(ramenName);
+  ramenDetailDiv.appendChild(ramenRestaurant);
+  if (ramen.rating) ramenDetailDiv.appendChild(ramenRating);
+  if (ramen.comment) ramenDetailDiv.appendChild(ramenComment);
+}
+
 displayRamens();
